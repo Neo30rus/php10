b432582399394f0596357d6b704c270383f54f69
alevur <alevure@yandex.ru>
2023-03-17 20:22:01 +0400

    LEO1

--- phpHTTP.php
+++ phpHTTP.php
@@ -1,9 +1,12 @@
 <?php
 var_dump($_POST);
+if (!empty($_POST['fio'])){
+    $fio = $_POST['fio'];
+}
 ?>
 <form method="post">
-    <input type="text"name="=fio">
-    <input type="text" name="fio">
+    <input type="text"name="=fio[firstname]">
+    <input type="text" name="fio[lastname]">
     <input type="submit">
     </form>
 
